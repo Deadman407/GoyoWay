1f20cc165c2310e7cdfad253d01d01ab3e576549
Deadman407 <dead.407.man@gmail.com>
2022-01-04 06:26:07 -0800

    Updated Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+
 
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width: 600px)')
+    const coordinates = { lat: 0, lng: 0 }
     return (
-        <div>
-            Map
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCIChylF3z77H0l4wFcIs0QyaYV_GAL81A' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+             
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
